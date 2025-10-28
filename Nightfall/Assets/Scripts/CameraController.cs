ea2982b95108b742daf421589e2d30dd5b1f0520
Diamondskeleton0 <iwilliams21212122@gmail.com>
2025-10-27 20:24:23 -0400

    Code Comments

--- Nightfall/Assets/Scripts/CameraController.cs
+++ Nightfall/Assets/Scripts/CameraController.cs
@@ -6,6 +6,8 @@ public class CameraController : MonoBehaviour
     [SerializeField] int mouseSens;
     [SerializeField] int vertLockMin, vertLockMax;
     [SerializeField] bool invertY;
+
+    // Toggle for third-person camera, this could most likely be moved to the actual player in case they need changes between first and third person (VERY LIKELY).
     [SerializeField] bool thirdPersonEnabled;
 
     float rotX;
@@ -25,6 +27,8 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         
+        // Most of this was covered before, but I'll comment on things that are new/important.
+
         float mouseX = Input.GetAxisRaw("Mouse X") * mouseSens * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSens * Time.deltaTime;
 
@@ -39,8 +43,10 @@ public class CameraController : MonoBehaviour
 
         rotX = Mathf.Clamp(rotX, vertLockMin, vertLockMax);
 
+        // Handles vertical mouse rotation.
         transform.localRotation = Quaternion.Euler(rotX, 0, 0);
 
+        // Handles horizontal mouse rotation.
         if (!thirdPersonEnabled) { 
 
             transform.parent.Rotate(Vector2.up * mouseX);
