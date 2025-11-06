907c634d026cedb1c437d5fbbf590b35636f2887
QuantumTutor <djtwen23@student.fullsail.edu>
2025-11-06 18:53:33 -0500

    Refactor player rotation and animator parameters
    Renamed mouse rotation variables to use private naming convention and updated animator parameters to use Velocity X and Velocity Z with smoothing. Removed unused animMoveSpeed and improved clarity in rotation and movement handling.

--- Nightfall/Assets/Scripts/Player/PlayerController.cs
+++ Nightfall/Assets/Scripts/Player/PlayerController.cs
@@ -25,17 +25,20 @@ namespace Catalyst.Player
         public bool isInverted;
         public bool isInventoryOpen;
 
-        private float mouseXRotation;
-        private float mouseYRotation;
+        private float _mouseXRotation;
+        private float _mouseYRotation;
+        private float _velocityX;
+        private float _velocityZ;
 
         [Header("Animation")]
-        private int animMoveSpeed;
         private int animJump;
         private int animGrounded;
         private int animSprinting;
         private int animAttack;
         private int animDodge;
         private int animDash;
+        private int animVelocityX;
+        private int animVelocityZ;
 
         private int jumpCount = 0;
 
@@ -47,12 +50,13 @@ namespace Catalyst.Player
             characterController = GetComponent<CharacterController>();
             SetupAnimator();
             thirdPersonCamera.gameObject.SetActive(false);
+
         }
 
         private void LateUpdate()
         {
             //if (ThirdPersonActive())
-            ApplyThirdPersonRotation(mouseYRotation, mouseXRotation);
+            ApplyThirdPersonRotation(_mouseYRotation, _mouseXRotation);
 
 
 
@@ -76,13 +80,16 @@ namespace Catalyst.Player
 
         private void SetupAnimator()
         {
-            animMoveSpeed = Animator.StringToHash("Speed");
+
             animJump = Animator.StringToHash("Jump");
             animGrounded = Animator.StringToHash("Grounded");
             animSprinting = Animator.StringToHash("Sprinting");
             animAttack = Animator.StringToHash("Attack");
             animDodge = Animator.StringToHash("Dodge");
             animDash = Animator.StringToHash("Dash");
+
+            animVelocityX = Animator.StringToHash("Velocity X");
+            animVelocityZ = Animator.StringToHash("Velocity Z");
 
 
 
@@ -215,16 +222,13 @@ namespace Catalyst.Player
 
             characterController.Move(_currentMovement * Time.deltaTime);
 
-            animator.SetFloat(animMoveSpeed, Mathf.Max(Mathf.Max(Mathf.Abs(_currentMovement.z), Mathf.Abs(_currentMovement.x)), Mathf.Abs(mouseXRotation)));
-            //ThirdPersonActive();
+            animator.SetFloat(animVelocityX, Mathf.SmoothDamp(animator.GetFloat(animVelocityX), _currentMovement.x, ref _velocityX, 0.1f));
+            animator.SetFloat(animVelocityZ, Mathf.SmoothDamp(animator.GetFloat(animVelocityZ), _currentMovement.z, ref _velocityZ, 0.1f));
         }
 
         private void ApplyHorizontalRotation(float rotationAmount)
         {
-
-
             transform.Rotate(0, rotationAmount, 0);
-
         }
 
 
@@ -257,13 +261,13 @@ namespace Catalyst.Player
                 return;
             }
 
-            mouseXRotation = playerInputHandler.RotationInput.x * playerData.MouseSensitivity * playerData.RotationSpeed;
-            mouseYRotation = playerInputHandler.RotationInput.y * playerData.MouseSensitivity;
+            _mouseXRotation = playerInputHandler.RotationInput.x * playerData.MouseSensitivity * playerData.RotationSpeed;
+            _mouseYRotation = playerInputHandler.RotationInput.y * playerData.MouseSensitivity;
 
 
 
-            ApplyHorizontalRotation(mouseXRotation);
-            ApplyVerticalRotation(mouseYRotation);
+            ApplyHorizontalRotation(_mouseXRotation);
+            ApplyVerticalRotation(_mouseYRotation);
 
         }
 
@@ -301,7 +305,7 @@ namespace Catalyst.Player
                 Debug.DrawRay(mainCamera.transform.position, mainCamera.transform.forward * playerData.InteractRange, Color.red, 1f);
 
                 if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out RaycastHit hit, playerData.InteractRange, ~ignoreLayer))
-                    {
+                {
                     TryOpenChest(hit);
                     // logging the collider the raycast hit //
                     Debug.Log("Hit " + hit.collider.name);
