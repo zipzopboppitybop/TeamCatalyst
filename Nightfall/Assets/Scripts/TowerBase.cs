25f825e14bf21a0ebc0f70654e15c6855a3b735d
Diamondskeleton0 <iwilliams21212122@gmail.com>
2025-11-05 22:52:55 -0500

    Real Tower Base

--- Nightfall/Assets/Scripts/TowerBase.cs
+++ Nightfall/Assets/Scripts/TowerBase.cs
@@ -1,16 +1,162 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class TowerBase : MonoBehaviour
 {
+
+    [SerializeField] enum TowerType { Crop, Defensive, Offensive };
+    [SerializeField] enum CropType { None, Carrot };
+    [SerializeField] enum OffenseType { None, Sprinkler };
+
+    [SerializeField] TowerType typeTower;
+    [SerializeField] CropType typeCrop;
+    [SerializeField] OffenseType typeOffense;
+
+    [SerializeField] int hp;
+    [SerializeField] int damage;
+
+    [SerializeField] float attSpeed;
+
+    [SerializeField] GameObject itemDrop;
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject shootPos;
+
+    bool isFullyGrown;
+    bool EnemyInRange;
+
+    int enemiesInRange;
+    [SerializeField] List<Transform> enemyPos;
+
+    float shootTime;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         
+        if (typeTower == TowerType.Offensive)
+        {
+
+            //enemyPos = new List<Transform>();
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (typeTower == TowerType.Offensive)
+        {        
+            shootTime += Time.deltaTime;
+
+            if (shootTime >= attSpeed)
+            {
+
+                Vector3 chosenEnemyPos = ChooseClosestPos();
+                Quaternion rot = Quaternion.LookRotation(chosenEnemyPos);
+                transform.rotation = rot;
+                Shoot();
+                shootTime = 0;
+
+            }
+        }
+
+    }
+
+    void Grow()
+    {
+
+        isFullyGrown = true;
+
+    }
+
+    Vector3 ChooseClosestPos()
+    {
+
+        float shortestDistance = 1000;
+        Vector3 closestPos = Vector3.zero;
+
+        for (int i = 0; i < enemyPos.Count; i++)
+        {
+
+            if ((enemyPos[i].position - transform.position).normalized.magnitude < shortestDistance)
+            {
+
+                shortestDistance = (enemyPos[i].position - transform.position).normalized.magnitude;
+                closestPos = enemyPos[i].position;
+
+            }
+
+        }
+
+        return closestPos;
+
+    }
+
+    void Shoot()
+    {
+
+        Instantiate(bullet, shootPos.transform.position, transform.rotation);
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Enemy"))
+        {
+
+            enemiesInRange++;
+            enemyPos.Add(other.transform);
+            CheckForEnemies();
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        if (other.CompareTag("Enemy"))
+        {
+
+            enemiesInRange--;
+            enemyPos.Remove(other.transform);
+            CheckForEnemies();
+
+        }
         
     }
+
+    void CheckForEnemies()
+    {
+
+        if (enemiesInRange > 0)
+        {
+            EnemyInRange = true;
+        }
+        else
+        {
+            EnemyInRange = false;
+        }
+
+    }
+
+    void TakeDamage(int amt)
+    {
+
+        hp -= amt;
+        if (hp <= 0)
+        {
+            if (typeTower == TowerType.Crop)
+                Instantiate(itemDrop, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+    }
+
 }
