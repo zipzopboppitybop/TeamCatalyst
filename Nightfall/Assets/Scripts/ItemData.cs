4828e92753bbbc51f87b108235079132baa15294
Brian Washington Jr <bw49078@gmail.com>
2025-11-07 10:05:03 -0600

    Can drop items the hotbar is selecting

--- Nightfall/Assets/Scripts/ItemData.cs
+++ Nightfall/Assets/Scripts/ItemData.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class ItemData : ScriptableObject
 {
     public Sprite Icon;
+    public GameObject dropPrefab;
     public string displayName;
     public int id;
     public float sellValue;
