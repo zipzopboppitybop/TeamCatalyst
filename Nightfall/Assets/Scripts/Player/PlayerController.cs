d3e3d0596054f7e3f9bf4f95e1d405522fce7eb5
QuantumTutor <djtwen23@student.fullsail.edu>
2025-11-15 16:07:31 -0500

    Add save/load system and UI integration
    Introduced SaveSystem for saving and loading player progress, including player position and data. Updated PlayerController with Save/Load methods and PlayerSaveData struct. Modified PauseMenuUI to show a save confirmation popup and handle save/load actions. Added respawnPosition to PlayerData and updated MainUI.uxml to include SavePopup UI.

--- Nightfall/Assets/Scripts/Player/PlayerController.cs
+++ Nightfall/Assets/Scripts/Player/PlayerController.cs
@@ -278,6 +278,27 @@ namespace Catalyst.Player
             }
         }
 
+        public void Save(ref PlayerSaveData data)
+        {
+            data.Position = transform.position;
+            data.playerData = playerData;
+        }
+
+        public void Load(ref PlayerSaveData data)
+        {
+            characterController.enabled = false;
+            transform.position = data.Position;
+            playerData = data.playerData;
+            characterController.enabled = true;
+        }
+
+    }
+
+    [System.Serializable]
+    public struct PlayerSaveData
+    {
+        public Vector3 Position;
+        public PlayerData playerData;
     }
 }
 
